516ce6398bdd042ed56d6d811055538b5b371095
pierrelefevre <pierre-lefevre@ens.univ-artois.fr>
2018-03-21 18:46:09 +0100

    footer css et html ajout des icons icomoon dossier fonts fichier icons.css pjax

--- config/head.php
+++ config/head.php
@@ -10,6 +10,7 @@
 
         <meta name="apple-mobile-web-app-capable" content="yes">
 
+
         <!-- Polices -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -20,8 +21,10 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
 
         <script src="js/jquery-3.2.1.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="js/jquery.pjax.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
 
         <script src="js/script.js"></script>
 
@@ -30,6 +33,7 @@
         <link href="style/header.css" rel="stylesheet">
         <link href="style/style.css" rel="stylesheet">
         <link href="style/footer.css" rel="stylesheet">
+        <link href="style/icons.css" rel="stylesheet">
         <!-- Demo styles -->
 
         <!-- Fin Ajouts HEAD -->
