1cf7ea963d851c49176de21f06e1b26e292a21a8
Saad <alami.saad.pro@gmail.com>
2018-04-04 19:44:29 +0200

    integrations diverse

--- vues/404.php
+++ vues/404.php
@@ -1,1 +1,7 @@
-Désolé, cette page est introuvable !
+<?php
+include("config/head.php");
+?>
+<a class="acc404" href="index.php?action=listeIngredients#slide1">Retour accueil</a>
+<img class="img404" src="img/icones/404.png">
+<h2 class="titre404">404</h2>
+<p class="p404">On ne rentre pas dans la cuisine du chef !</p>
