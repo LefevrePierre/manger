9a9ae4c553f56c8c58fd2566646edf314463b688
Saad <alami.saad.pro@gmail.com>
2018-04-05 21:00:39 +0200

    home pc

--- vues/accueil.php
+++ vues/accueil.php
@@ -28,8 +28,21 @@ $_SESSION['ing_checked']=array();
     <img class="home-logo" src="img/icones/logo.png" alt="">
     <p class="home-p">Qu'est ce qu'on mange ? <br> Nous répondons enfin à cette question éternelle ! <br> Vos ingrédients, nos recettes, vos plats.</p>
     <img class="home-iphone" src="img/icones/iphone.png" alt="">
-    <img class="home-blanc" src="img/icones/homeblanc.png" alt="">
+    <img class="home-forme" src="img/icones/homeblanc.png" alt="">
     <a href="index.php?action=listeIngredients" class="home-go">C'est parti ></a>
+</div>
+
+<div class="home-pc">
+    <img src="img/icones/fourchette.png" alt="" class="fourchette">
+    <div class="home-vert">
+        <img src="img/icones/logo.png" alt="">
+        <p>Webapplication</p>
+        <h1>Qu'est ce qu'on mange ?</h1>
+    </div>
+    <div class="home-blanc">
+        <h3>Rendez vous sur mobile</h3>
+        <p>Parce qu'on a rarement son ordinateur avec soi devant le frigo ou le placard, <strong>rendez-vous</strong> sur <strong>mobile</strong> dès maintenant ! <strong>Sélectionnez</strong> vos ingrédients. <strong>Découvrez</strong> nos recettes étudiantes. Du burger maison à la poêlée de légumes, un large choix vous attend. <strong>À partager</strong> seul ou entre amis !</p>
+    </div>
 </div>
 
 
